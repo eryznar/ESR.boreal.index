95a3a01ac50873bf033b9611351369486830a9bb
Emily Ryznar <emily.ryznar@nmfs.local>
2025-07-31 12:05:49 -0700

    adding crabpack functionality

--- Scripts/calc_coreimm_area.R
+++ Scripts/calc_coreimm_area.R
@@ -14,93 +14,60 @@
 
 source("./Scripts/load_libs_params.R")
 
-## EBS haul data ----
-sc_catch <- read.csv(paste0("Y:/KOD_Survey/EBS Shelf/", current.year, "/Tech Memo/Data/crabhaul_opilio.csv"))
-
-#EBS strata data ----
-sc_strata <- read.csv(paste0("Y:/KOD_Survey/EBS Shelf/", current.year, "/Tech Memo/Data/STRATA_OPILIO_NEWTIMESERIES.csv"))
-
+## EBS catch and haul data ----
+sc_dat <- crabpack::get_specimen_data(species = "SNOW", 
+                                        region = "EBS", 
+                                        years = c(1975:2019, 2021:current.year),
+                                        channel = "API")
+sc_catch <- sc_dat$specimen
+sc_haul <- sc_dat$haul
 
 ###################################################
 # data exploration ----
 
 #Stations sampled in each year
-sc_catch <- sc_catch %>%
-  mutate(AKFIN_SURVEY_YEAR = lubridate::year(lubridate::parse_date_time(START_DATE, orders = "mdy"))) 
-
 sc_catch %>%
-  group_by(AKFIN_SURVEY_YEAR) %>%
-  summarise(num_stations = length(unique(GIS_STATION)))
+  group_by(YEAR) %>%
+  summarise(num_stations = length(unique(STATION_ID)))
 
 #Plot pre-standardization data
 sc_catch %>%
-  filter(AKFIN_SURVEY_YEAR < 1988) %>%
-  group_by(AKFIN_SURVEY_YEAR, MID_LONGITUDE, MID_LATITUDE) %>%
-  distinct(GIS_STATION) %>%
+  filter(YEAR < 1988) %>%
+  group_by(YEAR, LONGITUDE, LATITUDE) %>%
+  distinct(STATION_ID) %>%
 ggplot() +
-  geom_point(aes(x = MID_LONGITUDE, y = MID_LATITUDE), size=.5) +
+  geom_point(aes(x =LONGITUDE, y = LATITUDE), size=.5) +
   labs(x = "Longitude", y = "Latitude") +
-  facet_wrap(~AKFIN_SURVEY_YEAR)
+  facet_wrap(~YEAR)
 
 #Earliest yrs don't survey prime snow crab habitat so 90% of habitat index
   #will be biased in these years 
 
-# Specify corner stations for exclusion
-corner <- list("QP2625","ON2625","HG2019","JI2120","IH1918",
-               "GF2221","HG1918","GF2019","ON2524","PO2726",
-               "IH2221","GF1918","JI2221","JI2019","JI1918",
-               "HG2221","QP2726","PO2423","IH2019","PO2625",
-               "QP2423","IH2120","PO2524","HG2120","GF2120",
-               "QP2524")
-
 #Calculate CPUE by station for all immature snow crab 
-sc_catch %>%
-  filter(HAUL_TYPE == 3, 
-         SEX %in% 1:2,
-         AKFIN_SURVEY_YEAR > 1979) %>%
-  mutate(MAT_SEX = case_when((SEX == 1 & WIDTH < 95 & SHELL_CONDITION <= 2) ~ "Immature Male",
-                             (SEX == 2 & CLUTCH_SIZE == 0) ~ "Immature Female")) %>%
-  filter(MAT_SEX %in% c("Immature Male", "Immature Female")) %>%
-  group_by(AKFIN_SURVEY_YEAR, GIS_STATION, AREA_SWEPT,MID_LATITUDE, MID_LONGITUDE, GEAR_TEMPERATURE) %>%
-  summarise(N_CRAB = sum(SAMPLING_FACTOR, na.rm = T),
-            CPUE = N_CRAB / mean(AREA_SWEPT)) %>%
-  ungroup() %>%
-  #join to zero catch stations
-  right_join(., sc_strata %>%
-                    filter(SURVEY_YEAR > 1979) %>%
-                    distinct(STATION_ID, SURVEY_YEAR, STRATUM, TOTAL_AREA) %>%
-                    rename_all(~c("GIS_STATION", "AKFIN_SURVEY_YEAR",
-                             "STRATUM", "TOTAL_AREA_SQ_NM")))  %>%
-              replace_na(list(CPUE = 0)) -> cpue
+cpue <- calc_cpue(crab_data = sc_dat,
+                  species = "SNOW",
+                  crab_category = c("all_categories"), # error when specifying small_male and immature_female directly
+                  rm_corners = TRUE, # exclude corner stations
+                  years = c(1975:2019, 2021:current.year),
+                  region = "EBS",
+                  shell_condition = c("soft_molting", "new_hardshell")) %>% # SC =<2
+        filter(CATEGORY %in% c("small_male", "immature_female")) %>% # only immature
+        group_by(YEAR, STATION_ID, LATITUDE, LONGITUDE) %>%
+        reframe(CPUE = sum(CPUE))
 
- # filter out corner stations
- cpue %>%
-   filter(!GIS_STATION %in% corner) -> cpue2
-  
 # Determine stations that compose average core habitat across the long-term timeseries 
-
-#Histogram with percentiles 
-cpue2 %>%
-  group_by(GIS_STATION) %>%
-  summarise(AVG_CPUE = mean(CPUE)) -> data
-
-p50 <- quantile(data$AVG_CPUE, 0.50)
-
 #stations in 50-100 CPUE percentile range
-cpue2 %>%
-  group_by(GIS_STATION) %>%
+cpue %>%
+  group_by(STATION_ID) %>%
   summarise(AVG_CPUE = mean(CPUE)) %>%
   filter(AVG_CPUE > quantile(AVG_CPUE, 0.50)) -> perc50 #174 stations
 #Lets go with the 50th percentile for defining core immature area 
 
 #Join lat/long back in to perc50 dataset and plot
-sc_strata %>%
-      filter(SURVEY_YEAR == 2021) %>% #Just selecting a yr when all stations were sampled
+cpue %>%
+      filter(YEAR == 2021) %>% #Just selecting a yr when all stations were sampled
       dplyr::select(STATION_ID, LATITUDE, LONGITUDE) %>%
-      dplyr::rename(GIS_STATION = STATION_ID) %>%
       right_join(perc50) -> perc50_core
 
 #Write csv for stations in 50th percentile of avg CPUE  
 write.csv(perc50_core, file="./Output/imm_area_50perc.csv")
-
-
